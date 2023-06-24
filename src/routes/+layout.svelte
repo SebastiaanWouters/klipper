13f4e567172730931297c22fb425fd265c0f49cd
SebastiaanWouters <sebastiaan.wouters@kuleuven.be>
2023-06-24 12:32:31 +0200

    add page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
   import { SvelteToast } from '@zerodevx/svelte-toast'
 </script>
 
+<svelte:head>
+    <title>Video Clipper</title> 
+</svelte:head>
+
+
 <div class="w-full min-h-screen flex justify-center items-center bg-[#111111] text-neutral-100">
   <SvelteToast />
   <slot />
